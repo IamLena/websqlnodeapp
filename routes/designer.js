4b1a633a9738f09ae6f3d1f5263bcc5c7d704d82
Lena <helly.luchina@gmail.com>
2020-08-01 17:32:10 +0300

    psd, tif

--- routes/designer.js
+++ routes/designer.js
@@ -23,56 +23,56 @@ db.connect((err) => {
 });
 
 router.get('/', DesignerController.personalPage);
-// router.get('/create', DesignerController.GETCreateRecord);
+router.get('/create', DesignerController.GETCreateRecord);
 router.post('/create', DesignerController.POSTCreateRecord);
 
-router.get('/create', (req, res) => {
-	let {code, content, height, width, ppi, grab, preview, psd, tif, lan_geo, os, device, login} = req.query;
-	let ossql;
-	let devsql;
-	if (os)
-	{
-		ossql = `select * from os where nickname = "${os}"`;
-		if (device) // not undefined if passing /?device=undefined
-			devsql = `select * from devices where nickname = "${device}"`;
-		else
-		{
-			devsql = `select devices.nickname as nickname, devices.name as name
-			from
-			devices inner join
-			(select *
-			from os_dev_comp
-			where os_nick = "${os}") tmp
-			on devices.nickname = tmp.dev_nick`
-		}
-	}
-	else
-	{
-		ossql = `select * from os`;
-		devsql = `select * from devices where nickname = "nodevice"`;
-	}
+// router.get('/create', (req, res) => {
+// 	let {code, content, height, width, ppi, grab, preview, psd, tif, lan_geo, os, device, login} = req.query;
+// 	let ossql;
+// 	let devsql;
+// 	if (os)
+// 	{
+// 		ossql = `select * from os where nickname = "${os}"`;
+// 		if (device) // not undefined if passing /?device=undefined
+// 			devsql = `select * from devices where nickname = "${device}"`;
+// 		else
+// 		{
+// 			devsql = `select devices.nickname as nickname, devices.name as name
+// 			from
+// 			devices inner join
+// 			(select *
+// 			from os_dev_comp
+// 			where os_nick = "${os}") tmp
+// 			on devices.nickname = tmp.dev_nick`
+// 		}
+// 	}
+// 	else
+// 	{
+// 		ossql = `select * from os`;
+// 		devsql = `select * from devices where nickname = "nodevice"`;
+// 	}
 
-	langeosql = lan_geo ? `select * from lan_geo where lan_geo = "${lan_geo}"` : `select * from lan_geo`;
+// 	langeosql = lan_geo ? `select * from lan_geo where lan_geo = "${lan_geo}"` : `select * from lan_geo`;
 
-	db.query(ossql, async (err, osresults) => {
-		if(err) throw err;
-		else {
-			db.query(devsql, async (err, devresults) => {
-				if (err) throw err;
-				else {
-					db.query(langeosql, async (err, lansresults) => {
-						if (err) throw err;
-						else res.render('designer/create', {
-							m_lans : lansresults,
-							m_oss : osresults,
-							m_devices : devresults
-						})
-					});
-				}
-			});
-		}
-	});
-});
+// 	db.query(ossql, async (err, osresults) => {
+// 		if(err) throw err;
+// 		else {
+// 			db.query(devsql, async (err, devresults) => {
+// 				if (err) throw err;
+// 				else {
+// 					db.query(langeosql, async (err, lansresults) => {
+// 						if (err) throw err;
+// 						else res.render('designer/create', {
+// 							m_lans : lansresults,
+// 							m_oss : osresults,
+// 							m_devices : devresults
+// 						})
+// 					});
+// 				}
+// 			});
+// 		}
+// 	});
+// });
 
 // router.post('/create', (req, res) => {
 // 	// login
